459cb0f26d89d1897720345c57692dbab2041cb8
dronmorse <raflitwinowicz@gmail.com>
2022-07-25 20:10:31 +0200

    adding small changes to BASIC_api.py

--- BASIC_api.py
+++ BASIC_api.py
@@ -2,11 +2,13 @@ import requests
 import json
 
 def formatted_print(data):
-    text = json.dumps(data, sort_keys=True, indent=3)
+    text = json.dumps(data.json(), sort_keys=True, indent=3)
     print (text)
 
 output = requests.get("https://api.github.com/users/dronmorse")
 
 print (output.status_code)
 print (output.json())
-formatted_print(output.json())
+formatted_print(output)
+print (output.json().keys())
+print (output.json()["site_admin"])
